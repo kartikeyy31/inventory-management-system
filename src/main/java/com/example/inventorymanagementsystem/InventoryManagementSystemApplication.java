3d964caf80cb12220f40c820caf566800bb4adf6
Kartikey Jaiswal <kartikeyjaiswal31@gmail.com>
2025-09-14 21:16:28 +0530

    pushing all uncommited code

--- src/main/java/com/example/inventorymanagementsystem/InventoryManagementSystemApplication.java
+++ src/main/java/com/example/inventorymanagementsystem/InventoryManagementSystemApplication.java
@@ -3,11 +3,18 @@ package com.example.inventorymanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 // Convention over Config, annotation
 @SpringBootApplication
 public class InventoryManagementSystemApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(InventoryManagementSystemApplication.class, args);
+		for (String bean: ctx.getBeanDefinitionNames()) {
+			Object obj = ctx.getBean(bean);
+			if (obj instanceof HandlerExceptionResolver) {
+				System.out.println(obj.getClass().getName() + "   KARTIKEY    ");
+			}
+		}
     }
 }
